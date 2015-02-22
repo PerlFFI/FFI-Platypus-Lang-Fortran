caf534fcf3cd824f5a05e0be6819db18d3c6c6e7
Graham Ollis <perl@wdlabs.com>
2015-02-22 07:40:13 -0500

    version 0.06

--- lib/FFI/Platypus/Lang/Fortran.pm
+++ lib/FFI/Platypus/Lang/Fortran.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use FFI::Platypus::Lang::Fortran::ConfigData;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 NAME
 
--- lib/Module/Build/FFI/Fortran/ExtUtilsF77.pm
+++ lib/Module/Build/FFI/Fortran/ExtUtilsF77.pm
@@ -48,7 +48,7 @@ variable F77LIBS, e.g.
 
 =cut
 
-our $VERSION = "0.05";
+our $VERSION = "0.06";
 
 BEGIN { eval q{ sub config_log {} } unless __PACKAGE__->can('config_log') }
 
